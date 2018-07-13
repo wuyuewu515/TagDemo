e93e1decfb68ae58355471da1b046a201ce99325
wuyuewu515 <946456206@qq.com>
2018-07-13 11:42:17 +0800

    <five_wu>1.删除无用类

--- app/src/main/java/com/example/user/flexboxlayoutdemo/DiyLongOnClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.flexboxlayoutdemo;
-
-/**
- * Created by user on 2018/7/13.
- */
-
-public interface DiyLongOnClickListener {
-    void diyOnLongClick(int position, TagInfo info);
-}
